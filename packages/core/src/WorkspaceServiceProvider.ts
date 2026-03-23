947ded8547860a4a2821b1464ddd60970e52cfc3
Valentin Panchenko <senior.konung@gmail.com>
2026-03-23 14:59:08 +0300

    feat: реорганизовать функции _makeDiscover и _makeInit для улучшения структуры и читаемости кода

--- packages/core/src/WorkspaceServiceProvider.ts
+++ packages/core/src/WorkspaceServiceProvider.ts
@@ -31,168 +31,169 @@ import { AbsolutePath, EntrypointConfig, RootConfig } from "./Models.js";
 import { type Workspace } from "./Workspace.js";
 import { WorkspaceService } from "./WorkspaceService.js";
 
+const _makeDiscover = Effect.gen(function* () {
+	const fs = yield* FileSystem.FileSystem;
+	const path = yield* Path.Path;
+	return Effect.fn("discover")(function* ($1: AbsolutePath | undefined) {
+		const startDir = AbsolutePath.makeUnsafe(path.resolve($1 ?? "."));
+
+		const findRootDir = (
+			dir: AbsolutePath,
+		): Effect.Effect<
+			Option.Option<AbsolutePath>,
+			PlatformError.PlatformError
+		> =>
+			Effect.gen(function* () {
+				const lockfileExists = yield* fs
+					.exists(path.resolve(dir, CONTEXT0_LOCK_FILE_NAME))
+					.pipe(Effect.orDie);
+				if (lockfileExists) return Option.some(dir);
+				else if (dir === "/") return Option.none();
+				else
+					return yield* findRootDir(
+						AbsolutePath.makeUnsafe(path.resolve(dir, "../")),
+					);
+			});
+
+		const rootDir = yield* findRootDir(startDir).pipe(
+			Effect.andThen(
+				Option.match({
+					onSome: Effect.succeed,
+					onNone: () =>
+						new RootDirNotFound({
+							startDir,
+						}).asEffect(),
+				}),
+			),
+		);
+
+		const rootConfig = yield* fs
+			.readFileString(path.resolve(rootDir, CONTEXT0_CONFIG_FILE_NAME))
+			.pipe(
+				Effect.andThen((content) => Effect.sync(() => load(content))),
+				Effect.andThen(SchemaParser.decodeUnknownEffect(RootConfig)),
+				Effect.catchIf(SchemaIssue.isIssue, (reason) =>
+					new InvalidRootConfig({
+						reason,
+					}).asEffect(),
+				),
+				Effect.when(
+					fs.exists(path.resolve(rootDir, CONTEXT0_CONFIG_FILE_NAME)),
+				),
+			);
+
+		const entrypoints = yield* pipe(
+			Effect.succeed(
+				Option.getOrElse(
+					Option.flatMap(rootConfig, ({ entrypoints }) => entrypoints),
+					() => [],
+				),
+			),
+			Effect.andThen(
+				Effect.forEach(
+					Effect.fnUntraced(function* (pattern) {
+						return yield* Effect.promise(() =>
+							glob(pattern, {
+								cwd: rootDir,
+								onlyDirectories: true,
+								dot: true,
+							}),
+						);
+					}),
+				),
+			),
+			Effect.map(Array.flatten),
+			Effect.andThen(
+				Effect.forEach(
+					Effect.fnUntraced(function* (dir) {
+						const dirPath = AbsolutePath.makeUnsafe(path.resolve(rootDir, dir));
+						const configPath = path.resolve(dirPath, CONTEXT0_CONFIG_FILE_NAME);
+						return {
+							dir: dirPath,
+							config: yield* fs.readFileString(configPath).pipe(
+								Effect.andThen((content) => Effect.sync(() => load(content))),
+								Effect.andThen(
+									SchemaParser.decodeUnknownEffect(EntrypointConfig),
+								),
+								Effect.catchIf(SchemaIssue.isIssue, (reason) =>
+									new InvalidEntrypointConfig({
+										reason,
+									}).asEffect(),
+								),
+								Effect.when(fs.exists(configPath)),
+							),
+						};
+					}),
+				),
+			),
+		);
+
+		const lockfile = yield* fs
+			.readFileString(path.resolve(rootDir, CONTEXT0_LOCK_FILE_NAME))
+			.pipe(
+				Effect.andThen((content) => Effect.sync(() => load(content))),
+				Effect.andThen(SchemaParser.decodeUnknownEffect(Lockfile)),
+				Effect.catchIf(SchemaIssue.isIssue, (reason) =>
+					new InvalidLockfile({
+						reason,
+					}).asEffect(),
+				),
+			);
+
+		return identity<Workspace>({
+			_tag: "Workspace",
+			entrypoints,
+			lockfile,
+			rootConfig,
+			rootDir,
+			cacheDir: AbsolutePath.makeUnsafe(
+				path.resolve(rootDir, CONTEXT0_CACHE_DIRECTORY),
+			),
+		});
+	});
+});
+
+const _makeInit = Effect.gen(function* () {
+	const fs = yield* FileSystem.FileSystem;
+	const path = yield* Path.Path;
+	const discover = yield* _makeDiscover;
+	return Effect.fn("init")(function* ($1: AbsolutePath | undefined) {
+		const rootDir = AbsolutePath.makeUnsafe(path.resolve($1 ?? "."));
+
+		const workspace = yield* discover(rootDir).pipe(
+			Effect.map(Option.some),
+			Effect.catchTag("RootDirNotFound", () => Effect.succeedNone),
+		);
+		if (workspace._tag === "Some") {
+			if (workspace.value.rootDir === rootDir) return workspace.value;
+			return yield* new RootDirAlreadyDefined({
+				currentDir: rootDir,
+				parentRootDir: workspace.value.rootDir,
+			});
+		}
+
+		yield* fs.writeFileString(
+			path.resolve(rootDir, CONTEXT0_LOCK_FILE_NAME),
+			CONTEXT0_LOCK_FILE_DEFAULT_CONTENT,
+		);
+
+		const configFile = path.resolve(rootDir, CONTEXT0_CONFIG_FILE_NAME);
+		yield* fs
+			.writeFileString(configFile, CONTEXT0_ROOT_CONFIG_FILE_DEFAULT_CONTENT)
+			.pipe(Effect.when(fs.exists(configFile).pipe(Effect.map(Boolean.not))));
+
+		return yield* discover(rootDir);
+	});
+});
+
 /**
  * @group Layers
  */
 export const layer = Layer.effect(
 	WorkspaceService,
 	Effect.gen(function* () {
-		const fs = yield* FileSystem.FileSystem;
-		const path = yield* Path.Path;
-
-		const discover = Effect.fn("discover")(function* (
-			$1: AbsolutePath | undefined,
-		) {
-			const startDir = AbsolutePath.makeUnsafe(path.resolve($1 ?? "."));
-
-			const findRootDir = (
-				dir: AbsolutePath,
-			): Effect.Effect<
-				Option.Option<AbsolutePath>,
-				PlatformError.PlatformError
-			> =>
-				Effect.gen(function* () {
-					const lockfileExists = yield* fs
-						.exists(path.resolve(dir, CONTEXT0_LOCK_FILE_NAME))
-						.pipe(Effect.orDie);
-					if (lockfileExists) return Option.some(dir);
-					else if (dir === "/") return Option.none();
-					else
-						return yield* findRootDir(
-							AbsolutePath.makeUnsafe(path.resolve(dir, "../")),
-						);
-				});
-
-			const rootDir = yield* findRootDir(startDir).pipe(
-				Effect.andThen(
-					Option.match({
-						onSome: Effect.succeed,
-						onNone: () =>
-							new RootDirNotFound({
-								startDir,
-							}).asEffect(),
-					}),
-				),
-			);
-
-			const rootConfig = yield* fs
-				.readFileString(path.resolve(rootDir, CONTEXT0_CONFIG_FILE_NAME))
-				.pipe(
-					Effect.andThen((content) => Effect.sync(() => load(content))),
-					Effect.andThen(SchemaParser.decodeUnknownEffect(RootConfig)),
-					Effect.catchIf(SchemaIssue.isIssue, (reason) =>
-						new InvalidRootConfig({
-							reason,
-						}).asEffect(),
-					),
-					Effect.when(
-						fs.exists(path.resolve(rootDir, CONTEXT0_CONFIG_FILE_NAME)),
-					),
-				);
-
-			const entrypoints = yield* pipe(
-				Effect.succeed(
-					Option.getOrElse(
-						Option.flatMap(rootConfig, ({ entrypoints }) => entrypoints),
-						() => [],
-					),
-				),
-				Effect.andThen(
-					Effect.forEach(
-						Effect.fnUntraced(function* (pattern) {
-							return yield* Effect.promise(() =>
-								glob(pattern, {
-									cwd: rootDir,
-									onlyDirectories: true,
-									dot: true,
-								}),
-							);
-						}),
-					),
-				),
-				Effect.map(Array.flatten),
-				Effect.andThen(
-					Effect.forEach(
-						Effect.fnUntraced(function* (dir) {
-							const dirPath = AbsolutePath.makeUnsafe(
-								path.resolve(rootDir, dir),
-							);
-							const configPath = path.resolve(
-								dirPath,
-								CONTEXT0_CONFIG_FILE_NAME,
-							);
-							return {
-								dir: dirPath,
-								config: yield* fs.readFileString(configPath).pipe(
-									Effect.andThen((content) => Effect.sync(() => load(content))),
-									Effect.andThen(
-										SchemaParser.decodeUnknownEffect(EntrypointConfig),
-									),
-									Effect.catchIf(SchemaIssue.isIssue, (reason) =>
-										new InvalidEntrypointConfig({
-											reason,
-										}).asEffect(),
-									),
-									Effect.when(fs.exists(configPath)),
-								),
-							};
-						}),
-					),
-				),
-			);
-
-			const lockfile = yield* fs
-				.readFileString(path.resolve(rootDir, CONTEXT0_LOCK_FILE_NAME))
-				.pipe(
-					Effect.andThen((content) => Effect.sync(() => load(content))),
-					Effect.andThen(SchemaParser.decodeUnknownEffect(Lockfile)),
-					Effect.catchIf(SchemaIssue.isIssue, (reason) =>
-						new InvalidLockfile({
-							reason,
-						}).asEffect(),
-					),
-				);
-
-			return identity<Workspace>({
-				_tag: "Workspace",
-				entrypoints,
-				lockfile,
-				rootConfig,
-				rootDir,
-				cacheDir: AbsolutePath.makeUnsafe(
-					path.resolve(rootDir, CONTEXT0_CACHE_DIRECTORY),
-				),
-			});
-		});
-
-		const init = Effect.fn("init")(function* ($1: AbsolutePath | undefined) {
-			const rootDir = AbsolutePath.makeUnsafe(path.resolve($1 ?? "."));
-
-			const workspace = yield* discover(rootDir).pipe(
-				Effect.map(Option.some),
-				Effect.catchTag("RootDirNotFound", () => Effect.succeedNone),
-			);
-			if (workspace._tag === "Some") {
-				if (workspace.value.rootDir === rootDir) return workspace.value;
-				return yield* new RootDirAlreadyDefined({
-					currentDir: rootDir,
-					parentRootDir: workspace.value.rootDir,
-				});
-			}
-
-			yield* fs.writeFileString(
-				path.resolve(rootDir, CONTEXT0_LOCK_FILE_NAME),
-				CONTEXT0_LOCK_FILE_DEFAULT_CONTENT,
-			);
-
-			const configFile = path.resolve(rootDir, CONTEXT0_CONFIG_FILE_NAME);
-			yield* fs
-				.writeFileString(configFile, CONTEXT0_ROOT_CONFIG_FILE_DEFAULT_CONTENT)
-				.pipe(Effect.when(fs.exists(configFile).pipe(Effect.map(Boolean.not))));
-
-			return yield* discover(rootDir);
-		});
-
+		const discover = yield* _makeDiscover;
+		const init = yield* _makeInit;
 		return {
 			init: flow(
 				init,
