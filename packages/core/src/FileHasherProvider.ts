c01a560241c67729ea65382087c606d78d45ce0a
Valentin Panchenko <senior.konung@gmail.com>
2026-03-23 14:38:47 +0300

    feat: улучшить реализацию хеширования файлов, оптимизировав структуру кода и добавив обработку зависимостей

--- packages/core/src/FileHasherProvider.ts
+++ packages/core/src/FileHasherProvider.ts
@@ -14,79 +14,80 @@ import * as Lockfile from "./Lockfile.js";
 import { type Scope, type WorkspacePath } from "./Models.js";
 import { type Workspace } from "./Workspace.js";
 
+const _makeHash = Effect.gen(function* () {
+	const fs = yield* FileSystem.FileSystem;
+	const path = yield* Path.Path;
+
+	const baseCache = new Map<string, Hash.Hash>();
+	const extendedCache = new WeakMap<Workspace, Map<WorkspacePath, Hash.Hash>>();
+
+	const hashFile = Effect.fnUntraced(function* (file: string) {
+		const cachedHash = baseCache.get(file);
+		if (cachedHash) return cachedHash;
+		return yield* fs.stream(file).pipe(Stream.orDie, Hash.fromStream);
+	});
+
+	return Effect.fn("hash")(function* (
+		workspace: Workspace,
+		file: WorkspacePath,
+		scope: Scope,
+	) {
+		const cachedHash = extendedCache.get(workspace)?.get(file);
+		if (cachedHash) return cachedHash;
+
+		const contextFiles = yield* Lockfile.fileContext(
+			workspace.lockfile,
+			file,
+			scope,
+		);
+
+		const contextHashes = yield* Effect.forEach(
+			contextFiles,
+			Effect.fnUntraced(function* (contextFile) {
+				const contextFileLockinfo = yield* Lockfile.fileInfo(
+					workspace.lockfile,
+					contextFile,
+				);
+				const dependencyHashes = yield* pipe(
+					Lockfile.dependencyGroups(
+						workspace.lockfile,
+						Option.getOrThrow(contextFileLockinfo.annotations),
+					),
+					Array.filter(HashSet.has(file)),
+					Array.flatMap(Array.fromIterable),
+					Effect.forEach(
+						Effect.fnUntraced(function* (depFile) {
+							return yield* hashFile(
+								path.resolve(workspace.rootDir, depFile.replace("/", ".")),
+							);
+						}),
+					),
+					Effect.map(Array.join(":")),
+				);
+				const contextBaseHash = yield* hashFile(
+					path.resolve(workspace.rootDir, contextFile.replace("/", ".")),
+				);
+				return Hash.make(`${contextBaseHash}:${dependencyHashes}`);
+			}),
+		).pipe(Effect.map(Array.join(":")));
+
+		const baseHash = yield* hashFile(
+			path.resolve(workspace.rootDir, file.replace("/", ".")),
+		);
+
+		if (contextHashes.length === 0) return baseHash;
+		return Hash.make(`${baseHash}:${contextHashes}`);
+	});
+});
+
 /**
  * @group Layers
  */
 export const layer = Layer.effect(
 	FileHasher,
 	Effect.gen(function* () {
-		const fs = yield* FileSystem.FileSystem;
-		const path = yield* Path.Path;
-
-		const baseCache = new Map<string, Hash.Hash>();
-		const extendedCache = new WeakMap<
-			Workspace,
-			Map<WorkspacePath, Hash.Hash>
-		>();
-
-		const hashFile = Effect.fnUntraced(function* (file: string) {
-			const cachedHash = baseCache.get(file);
-			if (cachedHash) return cachedHash;
-			return yield* fs.stream(file).pipe(Stream.orDie, Hash.fromStream);
-		});
-
 		return {
-			hash: Effect.fn("hash")(function* (
-				workspace: Workspace,
-				file: WorkspacePath,
-				scope: Scope,
-			) {
-				const cachedHash = extendedCache.get(workspace)?.get(file);
-				if (cachedHash) return cachedHash;
-
-				const contextFiles = yield* Lockfile.fileContext(
-					workspace.lockfile,
-					file,
-					scope,
-				);
-
-				const contextHashes = yield* Effect.forEach(
-					contextFiles,
-					Effect.fnUntraced(function* (contextFile) {
-						const contextFileLockinfo = yield* Lockfile.fileInfo(
-							workspace.lockfile,
-							contextFile,
-						);
-						const dependencyHashes = yield* pipe(
-							Lockfile.dependencyGroups(
-								workspace.lockfile,
-								Option.getOrThrow(contextFileLockinfo.annotations),
-							),
-							Array.filter(HashSet.has(file)),
-							Array.flatMap(Array.fromIterable),
-							Effect.forEach(
-								Effect.fnUntraced(function* (depFile) {
-									return yield* hashFile(
-										path.resolve(workspace.rootDir, depFile.replace("/", ".")),
-									);
-								}),
-							),
-							Effect.map(Array.join(":")),
-						);
-						const contextBaseHash = yield* hashFile(
-							path.resolve(workspace.rootDir, contextFile.replace("/", ".")),
-						);
-						return Hash.make(`${contextBaseHash}:${dependencyHashes}`);
-					}),
-				).pipe(Effect.map(Array.join(":")));
-
-				const baseHash = yield* hashFile(
-					path.resolve(workspace.rootDir, file.replace("/", ".")),
-				);
-
-				if (contextHashes.length === 0) return baseHash;
-				return Hash.make(`${baseHash}:${contextHashes}`);
-			}),
+			hash: yield* _makeHash,
 		};
 	}),
 );
